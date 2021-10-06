d74a4ed784c915798cfbd3c4fe840b0933c0de93
André Henrique <andreh3nrique@gmail.com>
2021-10-06 17:58:51 -0300

    app alcool ou gasolina final

--- input_in_flutter/alcool_gasolina/lib/dart.dart
+++ input_in_flutter/alcool_gasolina/lib/dart.dart
@@ -10,12 +10,48 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   TextEditingController _controllerAlcool = TextEditingController();
   TextEditingController _controllerGasolina = TextEditingController();
+  String _textoResultado = "";
+  void _calcular() {
+    double precoAlcool = double.tryParse(_controllerAlcool.text);
+    double precoGasolina = double.tryParse(_controllerGasolina.text);
+    if (precoAlcool == null || precoGasolina == null) {
+      setState(() {
+        _textoResultado =
+            "Invalido, digite numeros maiores que 0, e sem virgulas(,) utilize o ponto (.)";
+      });
+    } else {
+      /* 
+      Se o preço do álcool dividido pelo preço da gasolina for >= 0.7 é melhor abastecer com gasolina, se não é melhor utilizar alcool
+      */
+      if ((precoAlcool / precoGasolina) >= 0.7) {
+        setState(() {
+          _textoResultado = "Melhor abastecer com gasolina!";
+        });
+      } else {
+        setState(() {
+          _textoResultado = "Melhor abastecer com álcool!";
+        });
+      }
+
+      //o controller não precisa estar dentro de um setState para alterar na tela, ele já faz isso no automático.
+      // _limparCampos();
+    }
+  }
+
+  void _limparCampos() {
+    _controllerGasolina.text = "";
+    _controllerAlcool.text = "";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Alcool ou gasolina"),
         backgroundColor: Colors.blue,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: _limparCampos)
+        ],
       ),
       body: SingleChildScrollView(
         child: Container(
@@ -65,17 +101,14 @@ class _HomeState extends State<Home> {
                     "Calcular",
                     style: TextStyle(fontSize: 20),
                   ),
-                  onPressed: () {},
+                  onPressed: _calcular,
                 ),
               ),
               Padding(
                 padding: EdgeInsets.only(top: 20),
                 child: Text(
-                  "Resultado",
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold
-                  ),
+                  _textoResultado,
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                 ),
               )
             ],
