1314852577215dcd5a40dfcb1e7cdc4c0e6bbdde
André Henrique <andreh3nrique@gmail.com>
2021-10-16 11:34:19 -0300

    checkbox parte 2

--- input_in_flutter/conceitos_iniciais/lib/entradaCheckbox.dart
+++ input_in_flutter/conceitos_iniciais/lib/entradaCheckbox.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
- class EntradaCheckbox extends StatefulWidget {
-  // const EntradaCheckbox({ Key? key }) : super(key: key);
- 
-   @override
-   _EntradaCheckboxState createState() => _EntradaCheckboxState();
- }
- 
- class _EntradaCheckboxState extends State<EntradaCheckbox> {
-   
-   bool _estaSelecionado = false;
 
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-       appBar: AppBar(
+class EntradaCheckbox extends StatefulWidget {
+  // const EntradaCheckbox({ Key? key }) : super(key: key);
+
+  @override
+  _EntradaCheckboxState createState() => _EntradaCheckboxState();
+}
+
+class _EntradaCheckboxState extends State<EntradaCheckbox> {
+  bool _comidaBrasileira = false;
+  bool _comidaMexicana = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
         title: Text("Entrada de dados"),
         centerTitle: true,
       ),
-    body: Container(
-      child: Column(
-        children: <Widget>[
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            /*
+          checkbox metodo 1
+
           Text("Comida Brasileira"),
           Checkbox(
             value: _estaSelecionado,
@@ -30,10 +34,49 @@ import 'package:flutter/material.dart';
                 _estaSelecionado = valor;
               });
             },
-          ),
-        ],
+          ), */
+            CheckboxListTile(
+                title: Text("Comida brasileira"),
+                subtitle: Text("A melhor comida do mundo!!"),
+                //activeColor: Colors.red,
+                //selected: true,
+                //secondary: Icon(Icons.add_box),
+
+                value: _comidaBrasileira,
+                onChanged: (bool valor) {
+                  setState(() {
+                    _comidaBrasileira = valor;
+                  });
+                }),
+            CheckboxListTile(
+                title: Text("Comida mexicana"),
+                subtitle: Text("A melhor comida do mundo!!"),
+                //activeColor: Colors.red,
+                //selected: true,
+                //secondary: Icon(Icons.add_box),
+
+                value: _comidaMexicana,
+                onChanged: (bool valor) {
+                  setState(() {
+                    _comidaMexicana = valor;
+                  });
+                }),
+            RaisedButton(
+              onPressed: (){
+                print("Comida Brasileira: " + _comidaBrasileira.toString() + 
+                " Comida mexicana: " + _comidaMexicana.toString());
+
+              },
+              child: Text(
+                "Salvar",
+                style: TextStyle(
+                  fontSize: 20
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    ),
-     );
-   }
- }
+    );
+  }
+}
--- input_in_flutter/conceitos_iniciais/lib/main.dart
+++ input_in_flutter/conceitos_iniciais/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:entrada_dados_flutter/CampoTexto.dart';
 import 'package:entrada_dados_flutter/entradaCheckbox.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
 void main(){
   runApp(MaterialApp(
    // home: CampoTexto(),
